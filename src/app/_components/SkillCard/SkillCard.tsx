e8925c7e610aade73599488f89b81357240bbfa1
amir tejareh <amirtejareh@gmail.com>
2024-12-12 22:10:35 +0330

    refactor: change logo and text placement

--- src/app/_components/SkillCard/SkillCard.tsx
+++ src/app/_components/SkillCard/SkillCard.tsx
@@ -8,8 +8,8 @@ interface ISkill {
 const SkillCard: React.FC<ISkill> = ({ title, logo }) => {
   return (
     <div className="flex backdrop-blur-[10px] justify-center items-center w-[214.4px] gap-[8px] h-[72px] border-solid border-[1px] border-statCardBorder text-white rounded-[40px] bg-[linear-gradient(90deg,_rgba(33,33,30,0.1)_0%,_rgba(33,33,30,0.8)_100%)]">
-      <div> {title}</div>
       <div>{logo}</div>
+      <div> {title}</div>
     </div>
   );
 };
